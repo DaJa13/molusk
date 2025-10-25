b039d9192d3249ee41d7a09eb015ec836bee64a9
DaJa13 <vostokpro.com@gmail.com>
2025-10-25 17:37:24 +0300

    fix: добавил главную страницу

---
+++ app/page.js
@@ -0,0 +1,9 @@
+export default function Home() {
+    return (
+      <main style={{ textAlign: "center", padding: "40px" }}>
+        <h1>Привет! 🐚</h1>
+        <p>Сайт про моллюсков успешно работает!</p>
+      </main>
+    );
+  }
+  
